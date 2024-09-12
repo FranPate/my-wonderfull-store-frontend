53e1b4434cdebcc5623a419849da078131951e85
Francisco Patejim <jimpate0@gmail.com>
2024-09-12 04:13:06 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './index.css'
-import { AuthProvider } from './context/auth'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 )
